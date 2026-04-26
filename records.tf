4e891b3905d89d26523b177dfe3055781d87f676
aleksanderkaasik <aleks.kaasik@outlook.com>
2026-04-26 12:23:29 +0300

    Adding DNS records

--- records.tf
+++ records.tf
@@ -1,6 +1,6 @@
-# Default
+# DNS records
 resource "dns_a_record_set" "proxmox" {
-  zone = var.dns_zones["test"]
+  zone = var.dns_zones["Main"]
   name = "proxmox"
   addresses = [
     "192.168.1.253",
@@ -13,4 +13,54 @@ resource "dns_ptr_record" "proxmox_ptr" {
   name = "253"
   ptr  = "proxmox.test.home."
   ttl  = 300
+}
+
+# hosts
+resource "dns_a_record_set" "pki" {
+  zone = var.dns_zones["Main"]
+  name = "pki"
+  addresses = []
+  ttl = 300
+}
+
+resource "dns_a_record_set" "vault" {
+  zone = var.dns_zones["Main"]
+  name = "vault"
+  addresses = []
+  ttl = 300
+}
+
+resource "dns_a_record_set" "blog" {
+  zone = var.dns_zones["Blog"]
+  name = "www"
+  addresses = []
+  ttl = 300
+}
+
+resource "dns_a_record_set" "panel" {
+  zone = var.dns_zones["Main"]
+  name = "game-panel"
+  addresses = []
+  ttl = 300
+}
+
+resource "dns_a_record_set" "wing" {
+  zone = var.dns_zones["Main"]
+  name = "wing"
+  addresses = []
+  ttl = 300
+}
+
+resource "dns_a_record_set" "zabbix" {
+  zone = var.dns_zones["Main"]
+  name = "monitor"
+  addresses = []
+  ttl = 300
+}
+
+resource "dns_a_record_set" "cloud" {
+  zone = var.dns_zones["Main"]
+  name = "cloud"
+  addresses = []
+  ttl = 300
 }
