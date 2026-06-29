3c2165f58cc76c2922f6fcf184aa48ae2db03642
aleksanderkaasik <aleks.kaasik@outlook.com>
2026-06-29 16:58:03 +0300

    Adding more records to the list

--- records.tf
+++ records.tf
@@ -11,57 +11,189 @@ resource "dns_a_record_set" "proxmox" {
 resource "dns_ptr_record" "proxmox_ptr" {
   zone = var.dns_zones["192.168.1.0"]
   name = "253"
-  ptr  = "proxmox.test.home."
+  ptr  = "proxmox.kaasilius.com."
+  ttl  = 300
+} 
+
+resource "dns_ptr_record" "bind_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "45"
+  ptr  = "ns.kaasilius.com."
   ttl  = 300
 }
 
 resource "dns_a_record_set" "pki" {
-  zone      = var.dns_zones["Main"]
-  name      = "pki"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Main"]
+  name = "pki"
+  addresses = [
+    "192.168.1.41",
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "pki_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "41"
+  ptr  = "pki.kaasilius.com."
+  ttl  = 300
 }
 
 resource "dns_a_record_set" "vault" {
-  zone      = var.dns_zones["Main"]
-  name      = "vault"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Main"]
+  name = "vault"
+  addresses = [
+    "192.168.1.52",
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "vault_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "52"
+  ptr  = "vault.kaasilius.com."
+  ttl  = 300
 }
 
 resource "dns_a_record_set" "blog" {
-  zone      = var.dns_zones["Blog"]
-  name      = "www"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Blog"]
+  name = "aleksander"
+  addresses = [
+    "192.168.1.49",
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "blog_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "49"
+  ptr  = "aleksander.kaasik.blog."
+  ttl  = 300
 }
 
 resource "dns_a_record_set" "panel" {
-  zone      = var.dns_zones["Main"]
-  name      = "game-panel"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Main"]
+  name = "game-panel"
+  addresses = [
+    "192.168.1.43"
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "panel_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "41"
+  ptr  = "pki.kaasilius.com."
+  ttl  = 300
 }
 
 resource "dns_a_record_set" "wing" {
-  zone      = var.dns_zones["Main"]
-  name      = "wing"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Main"]
+  name = "wing"
+  addresses = [
+    "192.168.1.50"
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "wing_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "50"
+  ptr  = "wing.kaasilius.com."
+  ttl  = 300
 }
 
 resource "dns_a_record_set" "zabbix" {
-  zone      = var.dns_zones["Main"]
-  name      = "monitor"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Main"]
+  name = "monitor"
+  addresses = [
+    "192.168.1.42"
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "zabbix_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "42"
+  ptr  = "monitor.kaasilius.com."
+  ttl  = 300
 }
 
 resource "dns_a_record_set" "cloud" {
-  zone      = var.dns_zones["Main"]
-  name      = "cloud"
-  addresses = []
-  ttl       = 300
+  zone = var.dns_zones["Main"]
+  name = "cloud"
+  addresses = [
+    "192.168.1.3"
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "cloud_ptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "3"
+  ptr  = "cloud.kaasilius.com."
+  ttl  = 300
+}
+
+resource "dns_a_record_set" "dbtest" {
+  zone = var.dns_zones["Main"]
+  name = "dbtest"
+  addresses = [
+    "192.168.1.4"
+  ]
+  ttl = 300
+}
+
+resource "dns_ptr_record" "dbtestptr" {
+  zone = var.dns_zones["192.168.1.0"]
+  name = "4"
+  ptr  = "dbtest.kaasilius.com."
+  ttl  = 300
+}
+
+
+resource "dns_a_record_set" "aero_test" {
+  zone = var.dns_zones["Main"]
+  name = "aerotest"
+  addresses = [
+    "192.168.1.46"
+  ]
+  ttl = 300
+}
+
+resource "dns_a_record_set" "aeropack_test" {
+  zone = var.dns_zones["Main"]
+  name = "aeropack"
+  addresses = [
+    "192.168.1.46"
+  ]
+  ttl = 300
+}
+
+resource "dns_a_record_set" "zombieskytest" {
+  zone = var.dns_zones["Main"]
+  name = "zombieskytest"
+  addresses = [
+    "192.168.1.46"
+  ]
+  ttl = 300
+}
+
+resource "dns_a_record_set" "zombiepack" {
+  zone = var.dns_zones["Main"]
+  name = "zombiepack"
+  addresses = [
+    "192.168.1.46"
+  ]
+  ttl = 300
+}
+
+resource "dns_a_record_set" "zombiesky" {
+  zone = var.dns_zones["Main"]
+  name = "zombiesky"
+  addresses = [
+    "192.168.1.46"
+  ]
+  ttl = 300
 }
 
 # Cloudflare records
@@ -72,12 +204,12 @@ resource "cloudflare_dns_record" "Main_origin" {
   type    = "A"
   comment = "Domain verification record"
   content = local.home_ip
-  proxied = true
+  proxied = false
 }
 
-resource "cloudflare_dns_record" "vault" {
+resource "cloudflare_dns_record" "cloud" {
   zone_id = var.zones["Main"]
-  name    = "vault"
+  name    = "cloud"
   ttl     = 1
   type    = "CNAME"
   comment = "Domain verification record"
@@ -85,9 +217,9 @@ resource "cloudflare_dns_record" "vault" {
   proxied = true
 }
 
-resource "cloudflare_dns_record" "cloud" {
+resource "cloudflare_dns_record" "gamepanel" {
   zone_id = var.zones["Main"]
-  name    = "cloud"
+  name    = "game-panel"
   ttl     = 1
   type    = "CNAME"
   comment = "Domain verification record"
@@ -102,15 +234,66 @@ resource "cloudflare_dns_record" "blog_origin" {
   type    = "A"
   comment = "Domain verification record"
   content = local.home_ip
-  proxied = true
+  proxied = false
 }
 
-resource "cloudflare_dns_record" "blog" {
+resource "cloudflare_dns_record" "MainBlog" {
   zone_id = var.zones["Blog"]
-  name    = "www"
+  name    = "aleksander"
   ttl     = 1
   type    = "CNAME"
   comment = "Domain verification record"
   content = cloudflare_dns_record.blog_origin.name
-  proxied = true
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "wing" {
+  zone_id = var.zones["Main"]
+  name    = "wing"  
+  ttl     = 1
+  type    = "CNAME"
+  comment = "Domain verification record"
+  content = cloudflare_dns_record.Main_origin.name
+  proxied = false
+}
+
+
+resource "cloudflare_dns_record" "aero_test" {
+  zone_id = var.zones["Main"]
+  name    = "aerotest"
+  ttl     = 1
+  type    = "CNAME"
+  comment = "Domain verification record"
+  content = cloudflare_dns_record.Main_origin.name
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "aeropack_test" {
+  zone_id = var.zones["Main"]
+  name    = "aeropack"
+  ttl     = 1
+  type    = "CNAME"
+  comment = "Domain verification record"
+  content = cloudflare_dns_record.Main_origin.name
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "zombieskypack" {
+  zone_id = var.zones["Main"]
+  name    = "zombieskytest"  
+  ttl     = 1
+  type    = "CNAME"
+  comment = "Domain verification record"
+  content = cloudflare_dns_record.Main_origin.name
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "ZombieSky" {
+  zone_id = var.zones["Main"]
+  name    = "zombiesky"  
+  ttl     = 1
+  type    = "CNAME"
+  comment = "Domain verification record"
+  content = cloudflare_dns_record.Main_origin.name
+  proxied = false
 }
